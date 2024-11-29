5740000d87b6257be432a1155b6179c78f179282
ChupadorDERolasGrandes <00001079112364sp@al.educacao.sp.gov.br>
2024-11-29 13:29:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é facebook?',
+    'é uma rede social'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Cazaquistão?',
+    'A capital de Cazaquistão é a Astana'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    '2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz banana em Inglês?',
+    'banana'
+)
